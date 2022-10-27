80da5c273ab39f906827cb76cbdd30f488d37308
Theus <matheusth.dev@gmail.com>
2022-10-27 01:40:22 -0300

    feat: user model and serializer

--- users/models.py
+++ users/models.py
@@ -6,16 +6,27 @@ from django.db import models
 
 class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    username = models.CharField(max_value=30, unique=True)
+
     email = models.EmailField(max_value=100, unique=True)
     password = models.CharField(max_value=100)
+
     first_name = models.CharField(max_value=50)
     last_name = models.CharField(max_value=50)
     birthdate = models.DateField()
-    bio = models.TextField(null=True, blank=True)
-    is_active = models.BooleanField(default=False)
     profile_image = models.URLField(
         default="https://i.ibb.co/hM0XsT4/21-214439-free-high-quality-person-icon-default-profile-picture.png"
     )
 
+    bio = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(default=False)
+
     updated_at = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = [
+        "username",
+        "email",
+        "password",
+        "first_name",
+        "last_name",
+        "birthdate",
+    ]
---
+++ users/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "birthdate",
+            "profile_image",
+            "bio",
+            "is_active",
+            "is_superuser",
+            "date_joined",
+            "updated_at",
+        ]
+
+        read_only_fields = [
+            "id",
+            "is_active",
+            "is_superuser",
+            "date_joined",
+            "updated_at",
+        ]
+
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+            },
+        }
+
+    def create(self, validated_data):
+        model = self.Meta.model
+
+        instance = model.objects.create_user(**validated_data)
+
+        return instance
